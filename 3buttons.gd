81b63dc328356af29b790e40d83df7f815740979
redguy999 <Bigredguy05@hotmail.com>
2024-02-24 11:43:35 -0500

    gets 3 buttons working

---
+++ 3buttons.gd
@@ -0,0 +1,74 @@
+extends Node2D
+
+#All levels need to have a script on the highest node
+#This node will recieve the signal that the elements of the level emits
+#This signal will then be sent to the level handler from this node.
+# Called when the node enters the scene tree for the first time.
+var correctButton
+func _ready():
+	get_node("Timer").timeOver.connect(timesUp)
+	placeButtons()
+	makeAnswer()
+	pass # Replace with function body.
+signal gameState(Success)#True if you won, otherwise false.
+func timesUp():
+	gameState.emit(false)
+
+func gameWon():
+	gameState.emit(true)
+
+var random =  RandomNumberGenerator.new()
+
+func makeAnswer():
+	var picker = random.randi_range(0, 2)
+	correctButton=buttonCombos[0][picker]
+	var word = get_node("Label")
+	word.text=correctButton.to_upper()
+	word.modulate=correctButton
+	await get_tree().create_timer(.5).timeout
+	#A wait is required otherwise size will not have updated yet.
+	#set_defered might work but I'm not sure how i'd write that.
+	word.position.x+=(-word.size.x/2)
+
+var buttonCombos=[#Hardcoding it cause its quicker
+	["red","green","blue"],
+	["red","blue","green"],
+	["blue","red","green"],
+	["blue","green","red"],
+	["green","red","blue"],
+	["green","blue","red"]
+]
+
+func placeButtons():
+	random.randomize()
+	var picker = random.randi_range(0, 5)
+	var nodes = ["Left","Middle","Right"]
+	#get_node("Left/blue")
+	for i in range(nodes.size()):
+		get_node(nodes[i]+"/"+buttonCombos[picker][i]).visible=true
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_red_button_down():
+	if(correctButton=="red"):
+		gameWon()
+	else:
+		timesUp()
+
+
+func _on_blue_button_down():
+	if(correctButton=="blue"):
+		gameWon()
+	else:
+		timesUp()
+
+
+func _on_green_button_down():
+	if(correctButton=="green"):
+		gameWon()
+	else:
+		timesUp()
--- level_handler.gd
+++ level_handler.gd
@@ -3,36 +3,60 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 #Each level must be the same as its scene name do not include the file tag
-var Levels=["3 buttons","3 buttons Wrong color"]
+var Levels=["3buttons","3buttonsWrongColor"]
 var level=""
+var levelInstructions=[
+	"Click on the button that matches the text",
+	#both 3 button levels should have the same instructions.
+	"Click on the button that matches the text"
+]
+var instructions=""
 func _ready():
 	beginLevel()
 	pass # Replace with function body.
 var inst#need to hold this as global
 func beginLevel():#I'll fix these names, maybe.
+	get_node("levelComplete").visible=false
+	get_node("gameOver").visible=false
+	#TODO: randomly select a level.
+	level=Levels[0]#Temp
+	instructions=levelInstructions[0]
 	loadLevel()
-	#Insert getting the instructions here.
 	showInstructions()
-	await get_tree().create_timer(5.0).timeout#TL;DR: wait five seconds
+	connectLevel()
+	print(inst.get_children())
+	await get_tree().create_timer(3.0).timeout#TL;DR: wait three seconds
 	#That should be more than enough time to read the instructions.
+	print(inst.get_children())
 	get_node("Instructions").visible=false
+	print(inst.get_children())
 	#Need to connect to the completion node.
-	getLevelTimer().start()#starts the timer.
+	print(inst.get_children())
+	getLevelTimer().get_child(0).start()#starts the timer.
+func connectLevel():
+	inst.gameState.connect(endScreen)
+func endScreen(boole):
+	finishLevel()
+	if(boole):
+		get_node("levelComplete").visible=true
+	else:
+		get_node("gameOver").visible=true
 func loadLevel():
 	var newScene = load("res://"+level+".tscn")
 	inst= newScene.instantiate()
 	add_child(inst)
+	print(inst.get_children())#but works here.
 func showInstructions():#Assume that the level has already been added to the tree
 	get_node("Instructions").visible=true#This gets hidden at times.
-	get_node("Instructions/Control/Label").text="Testing"#Replace with instructions for level.
+	get_node("Instructions/Control/Label").text=instructions#Replace with instructions for level.
 func getLevelTimer():
-	for child in inst.getChildren():
+	for child in inst.get_children(true):#getChildren fails here
 		if(child.name=="Timer"):
-			return child
+			return child.get_child(0)
 func finishLevel():
 	getLevelTimer().levelFinished=true
 	inst.queue_free()#Remove the level
-	get_node('LevelComplete').visible=true
+	get_node('levelComplete').visible=true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
